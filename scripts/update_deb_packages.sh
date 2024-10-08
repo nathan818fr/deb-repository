00db621a028828851a3aaf9c7b6d66529e60fc2c
Nathan Poirier <nathan@poirier.io>
2024-10-08 02:37:56 +0200

    Remove ulauncher

--- scripts/update_deb_packages.sh
+++ scripts/update_deb_packages.sh
@@ -24,7 +24,6 @@ function main() {
   download_latest_gh_release mmdbinspect maxmind/mmdbinspect
   download_latest_gh_release rclone rclone/rclone dash
   download_latest_gh_release sops mozilla/sops
-  download_latest_gh_release ulauncher Ulauncher/Ulauncher
   download_latest_gh_release xclicker robiot/xclicker
   download_latest_gh_release yaru818-theme nathan818fr/yaru818
   download_from_apt onedrive https://download.opensuse.org/repositories/home:/npreining:/debian-ubuntu-onedrive/Debian_12 Packages
